478a8a06447d49ceafe20b577f7212d94f0f137d
KSamquasha Martin <ksmaquashamartin@ymail.com>
2022-06-05 23:40:57 -0500

    test deployment

--- deploy.js
+++ deploy.js
@@ -102,7 +102,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '54.198.62.227',
+      host: '54.234.163.127',
       username: 'ubuntu',
       privateKey: 'labsuser.pem',
     })
